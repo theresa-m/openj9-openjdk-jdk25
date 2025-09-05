ca2b5d9991157354b5fa86dd6cb60b3e893baea0
Theresa Mammarella <Theresa.T.Mammarella@ibm.com>
2025-09-05 11:01:41 -0400

    Revert "8357425: (fs) SecureDirectoryStream setPermissions should use fchmodat"
    This reverts commit 6cba4582f7290272e64d120cbc8594494605e18f.

--- src/java.base/unix/classes/sun/nio/fs/UnixSecureDirectoryStream.java
+++ src/java.base/unix/classes/sun/nio/fs/UnixSecureDirectoryStream.java
@@ -414,24 +414,15 @@ class UnixSecureDirectoryStream
                 if (!ds.isOpen())
                     throw new ClosedDirectoryStreamException();
 
-                int mode = UnixFileModeAttribute.toUnixMode(perms);
-                if (file == null)
-                    fchmod(dfd, mode);
-                else if (followLinks)
-                    fchmodat(dfd, file, mode, 0);
-                else if (fchmodatNoFollowSupported())
-                    fchmodat(dfd, file, mode, AT_SYMLINK_NOFOLLOW);
-                else {
-                    int fd = open();
-                    try {
-                        fchmod(fd, mode);
-                    } finally {
-                        if (fd >= 0)
-                            UnixNativeDispatcher.close(fd, e-> null);
-                    }
+                int fd = (file == null) ? dfd : open();
+                try {
+                    fchmod(fd, UnixFileModeAttribute.toUnixMode(perms));
+                } catch (UnixException x) {
+                    x.rethrowAsIOException(file);
+                } finally {
+                    if (file != null && fd >= 0)
+                        UnixNativeDispatcher.close(fd, e-> null);
                 }
-            } catch (UnixException x) {
-                x.rethrowAsIOException(file);
             } finally {
                 ds.readLock().unlock();
             }
